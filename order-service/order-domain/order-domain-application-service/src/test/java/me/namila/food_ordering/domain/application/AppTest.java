d38d837bbd4ebb87065c13b825a9f91ece2cb6f2
Namila Bandara <namila007@gmail.com>
2023-08-11 03:21:21 +0530

    init order service :smirk_cat:

---
+++ order-service/order-domain/order-domain-application-service/src/test/java/me/namila/food_ordering/domain/application/AppTest.java
@@ -0,0 +1,22 @@
+package me.namila.food_ordering.domain.application;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
---
+++ order-service/order-messaging/src/main/java/me/namila/food_ordering/messaging/App.java
@@ -0,0 +1,13 @@
+package me.namila.food_ordering.messaging;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
