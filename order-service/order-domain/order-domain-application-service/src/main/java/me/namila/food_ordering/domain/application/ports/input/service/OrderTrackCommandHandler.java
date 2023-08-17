f161aaef4596c7ca13fea9a9533de32094a07179
Namila Bandara <namila007@gmail.com>
2023-08-18 02:53:12 +0530

    fixed mvn issues

--- order-service/order-domain/order-domain-application-service/src/main/java/me/namila/food_ordering/domain/application/ports/input/service/OrderTrackCommandHandler.java
+++ order-service/order-domain/order-domain-application-service/src/main/java/me/namila/food_ordering/domain/application/ports/input/service/OrderTrackCommandHandler.java
@@ -1,15 +1,16 @@
 package me.namila.food_ordering.domain.application.ports.input.service;
 
+import org.springframework.stereotype.Component;
+
 import lombok.extern.slf4j.Slf4j;
 import me.namila.food_ordering.domain.application.dto.track.TrackOrderQuery;
 import me.namila.food_ordering.domain.application.dto.track.TrackOrderResponse;
-import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class OrderTrackCommandHandler {
 
-    public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery){
-
-    }
+  public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+    return null;
+  }
 }
